c619300638c65d89cfdee0233c13c54a57ee1cc8
MuradSharifzada <muradsharifzada@gmail.com>
2025-03-11 00:04:19 +0400

    Supplier

---
+++ src/main/java/project/_Supplier.java
@@ -0,0 +1,19 @@
+package project;
+
+import java.util.function.Supplier;
+
+public class _Supplier {
+    public static void main(String[] args) {
+
+        System.out.println(getDBConnectionUrl());
+        //Supplier
+        System.out.println(getDBConnectionUrl.get());
+
+    }
+
+    static Supplier<String> getDBConnectionUrl=()->"jdbc://localhost:5432/users";
+
+    static String getDBConnectionUrl(){
+        return "jdbc://localhost:5432/users";
+    }
+}
